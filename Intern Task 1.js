b453d6d0dab6a4a1946c5cecfc181f65f06249dc
samkirupakaran <124425629+samkirupakaran@users.noreply.github.com>
2024-10-31 08:17:39 +0530

    Add files via upload

---
+++ Intern Task 1.js
@@ -0,0 +1,9 @@
+// Scroll event for navbar background change
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
